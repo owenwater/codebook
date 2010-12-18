3b8db2b94a66549f5d5f8c9eed55af59116912ed
owenwater <owen200104@gmail.com>
2010-12-17 23:08:13 -0800

    first commit, add rectangle_cutting.cpp

---
+++ base.h
@@ -0,0 +1,23 @@
+#ifndef _BASE_H_
+#define _BASE_H_
+
+#include <vector>
+#include <cstdio>
+#include <set>
+#include <map>
+#include <ctime>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <algorithm>
+
+#define inputfilename "input"
+#define outputfilename "a.out"
+
+using namespace std;
+ 
+#endif
